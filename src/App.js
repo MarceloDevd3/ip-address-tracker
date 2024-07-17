c21087c12095e452cd49a08915fb6ffa6c0d614b
Italo Marcelo <MarceloDev2000@outlook.com>
2024-07-17 15:51:15 -0300

    ip-address-tracker-api-react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './assets/css/style.min.css';
+import Painel from "./components/Painel";
+//import Map from "./components/Map";
+
+
+function App() {
+
+
+  return (
+     <>
+      <Painel />
+      
+     </>
+  );
+}
+
+export default App;
---
+++ src/components/Painel.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import { Icon } from 'leaflet';
+import axios from "axios";
+import arrow from './images/icon-arrow.svg';
+
+export default function Painel() {
+
+  const [painelInfo, setPainelInfo] = useState({
+    CurrentIpAddress: "192.212.174.101",
+    CurrentLocation: "Brooklyn",
+    CurrentState: "NY 10001",
+    CurrentTimezone: "05:00",
+    CurrentIsp: "SpaceX Starlink"
+  })
+
+  const [locationCode, setLocationCode] = useState({
+    lt: 34.08057,
+    lg: -118.07285
+  })
+
+  const [ip, setIp] = useState('');
+  const [location, setLocation] = useState(null);
+
+  const fetchLocation = async () => {
+    try {
+      const response = await axios.get(`https://geo.ipify.org/api/v2/country,city,vpn?`, {
+        params: {
+          apiKey: 'at_tTE8Nfzn42Qc50LxlJktTGdfto6HZ', // Substitua pela sua chave de API
+          ipAddress: ip
+        }
+      });
+      const data = response.data
+      setLocation(response.data);
+      setPainelInfo({
+        CurrentIpAddress: `${data.ip}`,
+        CurrentLocation: `${data.location.city}`,
+        CurrentState: `${data.location.region} ${data.location.postalCode}`,
+        CurrentTimezone: data.location.timezone,
+        CurrentIsp: data.isp
+      });
+
+      setLocationCode({
+        lt: data.location.lat,
+        lg: data.location.lng
+      })
+
+
+    } catch (error) {
+      console.error('Erro ao buscar a localização:', error);
+      setLocation(null);
+    }
+  };
+
+  function subForm(e) {
+    e.preventDefault()
+  }
+
+  
+
+
+  const UpdateMap = () => {
+    const map = useMap();
+    useEffect(() => {
+      map.setView([locationCode.lt, locationCode.lg], 17);
+    }, [locationCode, map]);
+    return null;
+  };
+
+
+  return (
+    <main>
+      <div className="painel">
+        <div className="top-painel">
+        <h1 className="Painel--title">IP address Tracker</h1>
+        <form onSubmit={subForm}>
+          <label htmlFor="pesquisa" aria-description="search-form" ></label>
+          <input type="text"
+            value={ip}
+            placeholder="Search for any IP address or domain"
+            onChange={(e) => setIp(e.target.value)} />
+
+          <button aria-description="button to submit your search" className="btn-black" onClick={fetchLocation}> <img src={arrow} alt="arrow-icon"/></button>
+
+        </form>
+        </div>
+        <article id="Painel--information-zone">
+          <div className="info-1 info" >
+           <div>
+           <h2 className="Ip-address">Ip Address</h2>
+           <p className="data">{painelInfo.CurrentIpAddress}</p>
+           </div>
+            <div className="line">
+            <div className="dd">
+
+</div>
+            </div>
+          </div>
+          <div className="info-2 info">
+            <div>
+            <h2 className="location">Location</h2>
+            <p className="data">{painelInfo.CurrentLocation}, {painelInfo.CurrentState}</p>
+            </div>
+            <div className="line"> 
+            <div className="dd">
+
+</div>
+            </div>
+          </div>
+          <div className="info-3 info">
+            <div>
+            <h2 className="TimeZone">Timezone</h2>
+            <p className="data"> UTC  {painelInfo.CurrentTimezone}</p>
+            </div>
+            <div className="line">
+            <div className="dd">
+
+</div>
+            </div>
+          </div>
+          <div className="info-4 info">
+            <div>
+            <h2 className="Isp">Isp</h2>
+            <p className="data">{painelInfo.CurrentIsp}</p>
+            </div>
+            
+          </div>
+      </article>
+      </div>
+     
+      <div className="map">
+        <MapContainer center={[locationCode.lt, locationCode.lg]} zoom={17} >
+          <UpdateMap />
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[locationCode.lt, locationCode.lg]} icon={new Icon({
+            iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+            iconUrl: require("leaflet/dist/images/icon-location.png"),
+          })
+          }
+          >
+          </Marker>
+        </MapContainer>
+      </div>
+    </main>
+  )
+}
